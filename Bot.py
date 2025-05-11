e7105817c83da205fa5b614f685dfa81b890a090
alfa <logicalfa.mail@gmail.com>
2025-05-11 20:37:37 +0330

    send message

--- Bot.py
+++ Bot.py
@@ -6,6 +6,8 @@ bot = telebot.TeleBot("7678641734:AAGoZYh-x0evr3NLReW0ZXUo-cQxNAEOx1w")
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+
+    # send message
     bot.send_message(message.chat.id, "welcome to my robot")
 
 
