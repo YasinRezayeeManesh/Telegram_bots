64ffbf094549d52f2b5fe20fdbd09c66d85c1467
alfa <logicalfa.mail@gmail.com>
2025-05-11 20:49:59 +0330

    reply message

--- Bot.py
+++ Bot.py
@@ -6,9 +6,11 @@ bot = telebot.TeleBot("7678641734:AAGoZYh-x0evr3NLReW0ZXUo-cQxNAEOx1w")
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-
     # send message
-    bot.send_message(message.chat.id, "welcome to my robot")
+    # bot.send_message(message.chat.id, "welcome to my robot")
+
+    # reply message
+    bot.reply_to(message, 'hi friend')
 
 
 bot.polling()
