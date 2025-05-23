1e56801374496f105260946b28921d065fb72729
alfa <logicalfa.mail@gmail.com>
2025-05-23 12:43:47 +0330

    create db

--- movies_list.py
+++ movies_list.py
@@ -1,6 +1,17 @@
 import telebot
+import sqlite3
 
 bot = telebot.TeleBot("7967925856:AAFfzJpNV8R7iTwcHvWY-3XDEepV3zEumog")
+
+
+with sqlite3.connect('movies_list.db') as connection:
+    curser = connection.cursor()
+    curser.execute("""
+        CREATE TABLE IF NOT EXISTS movies(
+            id integer primary key,
+            title text
+        );
+    """)
 
 
 @bot.message_handler(commands=['add'])
