2cf0b5b244b338ccbdffb301d32165d1306529e8
alfa <logicalfa.mail@gmail.com>
2025-05-25 12:37:31 +0330

    dbug get_movie_db

--- movies_list.py
+++ movies_list.py
@@ -17,7 +17,7 @@ with sqlite3.connect('movies_list.db') as connection:
 def get_movie_db():
     connect = sqlite3.connect('movies_list.db')
     curs = connect.cursor()
-    curs.execute("Select name From movies_list")
+    curs.execute("Select title From movies_list")
     movies = [row[0] for row in curs.fetchall()]
     connect.close()
     return movies
