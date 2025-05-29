661f222a12f78136fa25f331e6c1c131922dfe07
alfa <logicalfa.mail@gmail.com>
2025-05-29 17:01:09 +0330

    create db (image_bot)

--- image_bot.py
+++ image_bot.py
@@ -1,6 +1,24 @@
 import telebot
+import os
+import sqlite3
 
 bot = telebot.TeleBot("8173824325:AAH1HRs2brPz8HXVHRE6SKm2ssxRTxqLhII")
+
+
+def create_db():
+    connection = sqlite3.connect("images.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS image(
+            id INTEGER PRIMARE KEY,
+            path TEXT
+        )
+    """)
+    connection.commit()
+    connection.close()
+
+
+create_db()
 
 
 bot.infinity_polling()
