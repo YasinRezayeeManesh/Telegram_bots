5c7f7d1644e9a2ca916988b6a5f0af59c481c61a
alfa <logicalfa.mail@gmail.com>
2025-05-29 17:03:49 +0330

    create dir photos (image_bot)

--- image_bot.py
+++ image_bot.py
@@ -5,6 +5,10 @@ import sqlite3
 bot = telebot.TeleBot("8173824325:AAH1HRs2brPz8HXVHRE6SKm2ssxRTxqLhII")
 
 
+if not os.path.exists("image_bot_photos"):
+    os.makedirs("image_bot_photos")
+
+
 def create_db():
     connection = sqlite3.connect("images.db")
     cursor = connection.cursor()
