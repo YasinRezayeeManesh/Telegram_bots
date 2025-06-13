77b1d13ded1c5fd6047700e5b5c9ebcd024daf53
alfa <logicalfa.mail@gmail.com>
2025-06-13 21:53:40 +0330

    create main_menu (menu_bot)

--- menu_bot.py
+++ menu_bot.py
@@ -5,4 +5,12 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 bot = TeleBot("8033272065:AAE3D4XCamABh_Cx4i7coXWYWD4bMCj9Sc0")
 
 
+def main_menu():
+    markup = InlineKeyboardMarkup()
+    button_1 = InlineKeyboardButton('Menu 1', callback_data='menu_1')
+    button_2 = InlineKeyboardButton('Menu 2', callback_data='menu_2')
+    markup.add(button_1, button_2)
+    return markup
+
+
 bot.infinity_polling()
