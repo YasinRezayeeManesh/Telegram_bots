2ad40ac101ecb07561a9a514d8b9127c0391ece4
alfa <logicalfa.mail@gmail.com>
2025-06-13 22:06:27 +0330

    create submenu_1 (menu_bot)

--- menu_bot.py
+++ menu_bot.py
@@ -7,9 +7,18 @@ bot = TeleBot("8033272065:AAE3D4XCamABh_Cx4i7coXWYWD4bMCj9Sc0")
 
 def main_menu():
     markup = InlineKeyboardMarkup()
-    button_1 = InlineKeyboardButton('Menu 1', callback_data='menu_1')
-    button_2 = InlineKeyboardButton('Menu 2', callback_data='menu_2')
+    button_1 = InlineKeyboardButton('Menu 1', callback_data='menu 1')
+    button_2 = InlineKeyboardButton('Menu 2', callback_data='menu 2')
     markup.add(button_1, button_2)
+    return markup
+
+
+def sub_menu_1():
+    markup = InlineKeyboardMarkup(row_width=1)
+    button_1 = InlineKeyboardButton('submenu 1-1', callback_data='submenu 1-1')
+    button_2 = InlineKeyboardButton('submenu 1-2', callback_data='submenu 1-2')
+    return_button = InlineKeyboardButton('back', callback_data='back to main menu')
+    markup.add(button_1, button_2, return_button)
     return markup
 
 
