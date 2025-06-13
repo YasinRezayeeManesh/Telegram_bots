1f9c71ad8a955467f91fbee1c9b9651be3833896
alfa <logicalfa.mail@gmail.com>
2025-06-13 21:47:50 +0330

    Create menu_bot

---
+++ menu_bot.py
@@ -0,0 +1,8 @@
+from telebot import TeleBot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+bot = TeleBot("8033272065:AAE3D4XCamABh_Cx4i7coXWYWD4bMCj9Sc0")
+
+
+bot.infinity_polling()
