a44065df1a367fd1139913b903b71667b4787bee
alfa <logicalfa.mail@gmail.com>
2025-06-14 11:38:32 +0330

    create submenu 2-1 (menu_bot)

--- menu_bot.py
+++ menu_bot.py
@@ -47,6 +47,14 @@ def sub_menu_2():
     return markup
 
 
+def submenu_2_1():
+    markup = InlineKeyboardMarkup(row_width=1)
+    button_1 = InlineKeyboardButton('button 2-1', callback_data='button')
+    return_button = InlineKeyboardButton('back', callback_data='back to submenu 2')
+    markup.add(button_1, return_button)
+    return markup
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Please Chose Menu", reply_markup=main_menu())
